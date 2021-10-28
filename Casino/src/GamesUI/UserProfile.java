ef28fb91161ca3fa9bbf2a5466b439b2e2aab5e2
bctod <Bogdan200!>
2021-10-28 21:05:59 +0200

    Basics

--- Casino/src/GamesUI/UserProfile.java
+++ Casino/src/GamesUI/UserProfile.java
@@ -8,6 +8,16 @@ import java.awt.event.ActionListener;
 public class UserProfile {
     private JButton button1;
     private JPanel UserPannel;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JButton button7;
+    private JButton button8;
+    private JButton button9;
+    private JButton button10;
+    private JButton button11;
     private JFrame userProfileFrame;
 
     private MainMenu mainMenuframe;
