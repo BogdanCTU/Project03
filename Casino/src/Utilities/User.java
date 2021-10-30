8ac2ec68f74802f97400f2f271897a4769d2c279
bctod <Bogdan200!>
2021-10-30 20:09:20 +0200

    Added user class.

--- Casino/src/Utilities/User.java
+++ Casino/src/Utilities/User.java
@@ -1,2 +1,26 @@
-package Utilities;public class User {
+package Utilities;
+
+import javax.swing.*;
+
+public class User {
+    // VARIABLES
+    private String userName;
+    private Icon userIcon;
+    public static int userMoney;
+
+    // CONSTRUCTOR
+    public User(String userName, Icon userIcon){
+        this.userName = userName;
+        this.userIcon = userIcon;
+    }
+
+    // METHODS
+    public String getUserName(){ return this.userName; };
+    public Icon getUserIcon(){ return this.userIcon; };
+
+    public String ToString() {
+        return getUserName() + getUserIcon();
+    }
+
+    // EOF
 }
--- Casino/src/Utilities/UserImages.java
+++ Casino/src/Utilities/UserImages.java
@@ -1,2 +1,11 @@
-package Utilities;public class UserImages {
+package Utilities;
+
+import javax.swing.*;
+
+public class UserImages {
+    Icon c1 = new ImageIcon("C:\\Users\\bctod\\Desktop\\Proiect_03\\Casino\\src\\CharacterImages\\c1.png");
+
+
+
+
 }
